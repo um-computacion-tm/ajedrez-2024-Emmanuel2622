c348f734da46a8aaa1a704554fba70751106bd53
Carlos Emmanuel Denis <ce.denis@alumno.um.edu.ar>
2024-08-17 15:59:11 -0300

    Se agrego {piece = self.__board__.get_piece(from_row, from_col), self.change_turn()} en el else que comprueba si los valores ingresados en la funcion move son mayores 0 o menores a 7, y se agregaron mas test

--- test_chess.py
+++ test_chess.py
@@ -5,26 +5,47 @@ class Test_Chess(unittest.TestCase):
     def setUp(self):
         self.__chess__ = Chess()
 
-    def test_move(self):        
+    def test_move_from_col(self):        
         result = self.__chess__.move(0,8,0,0)
         print(result)
         self.assertEqual(
-            result, "error value"
+            result, "Error value from_col, out range"
         )
 
-    def test_move2(self):
+    def test_move_to_col(self):
+        result = self.__chess__.move(3,4,1,-6)
+        print(result)
+        self.assertEqual(
+            result, "Error value to_col, out range"
+        )
+
+    def test_move_from_row(self):
+        result = self.__chess__.move(9,2,0,0)
+        print(result)
+        self.assertEqual(
+            result, "Error value from_row, out range"
+        )
+
+    def test_move_to_row(self):
+        result = self.__chess__.move(5,2,9,0)
+        print(result)
+        self.assertEqual(
+            result, "Error value to_row, out range"
+        )
+
+    def test_move_pass(self):
         result = self.__chess__.move(0,2,0,0)
         print(result)
         self.assertEqual(
             result, "pass"
         )
 
-    def test_move3(self):
-        result = self.__chess__.move(0,9,0,0)
+    def test_move_valor_no_numerico(self):
+        result = self.__chess__.move(0,"hola",0,0)
         print(result)
         self.assertEqual(
-            result, "error value"
-        )
+            result, "error: no se ingreso un valor numerico"
+        )    
 
     def test_turn(self):
         result = self.__chess__.turn
@@ -32,5 +53,24 @@ class Test_Chess(unittest.TestCase):
         self.assertEqual(
             result, "WHITE"
         )
+
+    def test_change_turn_black(self):
+        result = self.__chess__.change_turn()
+        self.__chess__.__turn__ = "WHITE"
+        print(result)
+        self.assertEqual(
+            result, "BLACK"
+        )
+
+    def test_change_turn_white(self):
+        self.__chess__.change_turn()
+
+        result = self.__chess__.change_turn()
+        self.__chess__.__turn__ = "BLACK"
+        print(result)
+        self.assertEqual(
+            result, "WHITE"
+        )
+
 if __name__ == "__main__":
     unittest.main()
