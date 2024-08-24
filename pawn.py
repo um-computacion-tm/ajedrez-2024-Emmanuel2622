c51ac2f3ba7c970a300e920154fc920074ba51ee
Carlos Emmanuel Denis <ce.denis@alumno.um.edu.ar>
2024-08-24 01:44:22 -0300

    Incorporacion del salto doble para la pieza pawn

--- pawn.py
+++ pawn.py
@@ -3,7 +3,8 @@ from pieces import Piece
 class Pawn(Piece):
     def __init__(self, color):
         super().__init__(color)
-
+        self.__first_move__ = True
+        
     def __str__(self):
         if self.__color__ == "WHITE":
                 return "♙"
@@ -17,11 +18,21 @@ class Pawn(Piece):
             direction = 1
 
         # Validacion movimiento simple hacia adelante
-        if from_col == to_col and to_row == from_row + direction:            
+        if from_col == to_col and to_row == from_row + direction:
+            #Verifica que no tenga ninguna pieza adelante
             if board.get_piece(to_row, to_col) is None:
                 return True
-        return False
 
+        # Validacion movimiento doble hacia adelante    
+        if from_col == to_col and to_row == from_row + 2 * direction and self.__first_move__:
+            if board.get_piece(to_row, to_col) is None:
+                if (self.__color__ == "WHITE" and from_row == 6) or (self.__color__ == "BLACK" and from_row == 1):
+                    #Verifica que no tenga ninguna pieza adelante
+                    if board.get_piece(from_row + direction, from_col) is None:
+                        return True
+
+        return False
+    
     def move(self, from_row, from_col, to_row, to_col, board):
         if self.type_move(from_row, from_col, to_row, to_col, board):
             board.__positions__[to_row][to_col] = board.__positions__[from_row][from_col]
