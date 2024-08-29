03664025aed2f9dadf7c00dd25681e852650b960
Carlos Emmanuel Denis <ce.denis@alumno.um.edu.ar>
2024-08-28 21:16:21 -0300

    Se elimino la variable __first_move__ de la clase peon y se paso a piece, y se agrego que luego de ejecutar su primer movimiento la variable __first_move__ sea False

--- pawn.py
+++ pawn.py
@@ -1,10 +1,6 @@
 from pieces import Piece
 
-class Pawn(Piece):
-    def __init__(self, color):
-        super().__init__(color)
-        self.__first_move__ = True
-        
+class Pawn(Piece):        
     def __str__(self):
         if self.__color__ == "WHITE":
                 return "♙"
@@ -21,10 +17,12 @@ class Pawn(Piece):
         if from_col == to_col and to_row == from_row + direction:
             #Verifica que no tenga ninguna pieza adelante
             if board.get_piece(to_row, to_col) is None:
+                print("salto")
                 return True
 
         # Validacion movimiento doble hacia adelante    
-        if from_col == to_col and to_row == from_row + 2 * direction and self.__first_move__:
+        if from_col == to_col and to_row == from_row + 2 * direction and self.__first_move__ == True:
+            print("salto doble")
             if board.get_piece(to_row, to_col) is None:
                 if (self.__color__ == "WHITE" and from_row == 6) or (self.__color__ == "BLACK" and from_row == 1):
                     #Verifica que no tenga ninguna pieza adelante
@@ -42,6 +40,7 @@ class Pawn(Piece):
         if self.type_move(from_row, from_col, to_row, to_col, board):
             board.__positions__[to_row][to_col] = board.__positions__[from_row][from_col]
             board.__positions__[from_row][from_col] = None
+            self.__first_move__ = False
             return True
         else:
             return "Error en el movimiento"
