4afdb6d97dfea8004f9a7ef047ade1af8a4f5676
Carlos Emmanuel Denis <ce.denis@alumno.um.edu.ar>
2024-08-30 22:16:56 -0300

    Se creo el movimiento Vertical de la torre

--- rook.py
+++ rook.py
@@ -9,21 +9,33 @@ class Rook(Piece):
      
     def type_move(self, from_row, from_col, to_row, to_col, board):
 
+        #Verificamos la direccion del movimiento
+        if to_col > from_col or to_row > from_row:
+            step = 1
+        else:
+            step = -1
+
         #Movimiento Horizontal
         if from_row == to_row:
-            #Verificamos la direccion del movimiento
-            if to_col > from_col:
-                step = 1
-            else:
-                step = -1
-            
-            #Verifica que no haya piezas en el camino
+
+            #Verifica que no haya piezas en el la fila
             for col in range(from_col + step, to_col, step):
                 if board.get_piece(from_row, col) is not None:
                     return False
             
             #Verifica que en el destino no haya piezas y si hay que sean de otro color
             if board.get_piece(to_row, to_col) is None or board.get_piece(to_row, to_col).__color__ != self.__color__: 
+                return True
+
+        #Movimiento Vertical
+        if to_col == from_col:
+            #Verifica que no haya piezas en el la columna
+            for row in range(from_row + step, to_row, step):
+                if board.get_piece(row, from_col) is not None:
+                    return False
+            
+            #Verifica que en el destino no haya piezas y si hay que sean de otro color
+            if board.get_piece(to_row,to_col) is None or board.get_piece(to_row, to_col).__color__ != self.__color__:
                 return True
 
         return False
