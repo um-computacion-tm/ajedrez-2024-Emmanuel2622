9735c3a9b4912c7ecf044b818fc85f2ea261ba87
Carlos Emmanuel Denis <ce.denis@alumno.um.edu.ar>
2024-09-04 22:08:37 -0300

    Se agrego la funcion Move

--- bishop.py
+++ bishop.py
@@ -28,5 +28,13 @@ class Bishop(Piece):
             #Verifica que en el destino no haya piezas y si hay que sean de otro color
             if board.get_piece(to_row, to_col) is None or board.get_piece(to_row, to_col).__color__ != self.__color__: 
                 return True
-            
+                    
         return False
+
+    def move(self, from_row, from_col, to_row, to_col, board):
+        if self.type_move(from_row, from_col, to_row, to_col, board):
+            board.__positions__[to_row][to_col] = board.__positions__[from_row][from_col]
+            board.__positions__[from_row][from_col] = None
+            return True
+        else:
+            return "Error en el movimiento"
