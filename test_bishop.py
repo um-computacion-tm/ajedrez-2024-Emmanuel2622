2012250b4381d604e344b2acb5548dd364c39c28
Carlos Emmanuel Denis <ce.denis@alumno.um.edu.ar>
2024-09-10 20:11:24 -0300

    Se creo los test para la pieza bishop

--- test_bishop.py
+++ test_bishop.py
@@ -7,39 +7,94 @@ class Test_Bishop(unittest.TestCase):
     def test_move_good(self):
         self.__board__ = Board()
         self.__pawn__white__ = Pawn("WHITE")
-        self.__pawn__black__ = Pawn("WHITE")
+        self.__pawn__black__ = Pawn("BLACK")
         self.__bishop__ = Bishop("WHITE")
 
-        self.__pawn__white__.move(6, 2, 4, 2, self.__board__)
+        self.__pawn__white__.move(6, 3, 4, 3, self.__board__)
         self.__pawn__black__.move(1, 0, 3, 0, self.__board__)
         
-        result = self.__bishop__.move(7,1,6,2,self.__board__)
+        result = self.__bishop__.move(7,2,6,3,self.__board__)
+        
         self.assertEqual(result, True)
         print(self.__board__.__str__())
-
 
     def test_move_good_2(self):
         self.__board__ = Board()
         self.__pawn__white__ = Pawn("WHITE")
-        self.__pawn__black__ = Pawn("WHITE")
+        self.__pawn__black__ = Pawn("BLACK")
         self.__bishop__ = Bishop("WHITE")
 
-        self.__pawn__white__.move(6, 2, 4, 2, self.__board__)
+        self.__pawn__white__.move(6, 1, 4, 1, self.__board__)
         self.__pawn__black__.move(1, 0, 3, 0, self.__board__)
         
-        result = self.__bishop__.move(7,1,5,3,self.__board__)
+        result = self.__bishop__.move(7,2,5,0,self.__board__)
+        self.assertEqual(result, True)
+        print(self.__board__.__str__())
+
+    def test_move_good_3(self):
+        self.__board__ = Board()
+        self.__pawn__white__ = Pawn("WHITE")
+        self.__pawn__black__ = Pawn("BLACK")
+        self.__bishop__ = Bishop("BLACK")
+
+        self.__pawn__white__.move(6, 0, 4, 0, self.__board__)
+        self.__pawn__black__.move(1, 3, 2, 3, self.__board__)
+        self.__pawn__white__.move(4, 1, 5, 1, self.__board__)
+        
+        result = self.__bishop__.move(0,2,3,5,self.__board__)
         self.assertEqual(result, True)
         print(self.__board__.__str__())
 
     def test_move_bad(self):
         self.__board__ = Board()
         self.__pawn__white__ = Pawn("WHITE")
-        self.__pawn__black__ = Pawn("WHITE")
+        self.__pawn__black__ = Pawn("BLACK")
         self.__bishop__ = Bishop("WHITE")
 
         self.__pawn__white__.move(6, 2, 4, 2, self.__board__)
         self.__pawn__black__.move(1, 0, 3, 0, self.__board__)
         
-        result = self.__bishop__.move(7,1,6,3,self.__board__)
+        result = self.__bishop__.move(7,2,6,3,self.__board__)
         self.assertEqual(result, "Error en el movimiento")
         print(self.__board__.__str__())
+
+    def test_move_bad_2(self):
+        self.__board__ = Board()
+        self.__pawn__white__ = Pawn("WHITE")
+        self.__pawn__black__ = Pawn("BLACK")
+        self.__bishop__ = Bishop("WHITE")
+
+        self.__pawn__white__.move(6, 2, 4, 2, self.__board__)
+        self.__pawn__black__.move(1, 0, 3, 0, self.__board__)
+        
+        result = self.__bishop__.move(7,2,5,4,self.__board__)
+        self.assertEqual(result, "Error en el movimiento")
+        print(self.__board__.__str__())
+
+
+    def test_move_bad_3(self):
+        self.__board__ = Board()
+        self.__pawn__white__ = Pawn("WHITE")
+        self.__pawn__black__ = Pawn("BLACK")
+        self.__bishop__ = Bishop("WHITE")
+
+        self.__pawn__white__.move(6, 2, 4, 2, self.__board__)
+        self.__pawn__black__.move(1, 0, 3, 0, self.__board__)
+        
+        result = self.__bishop__.move(7,2,3,4,self.__board__)
+        self.assertEqual(result, "Error en el movimiento")
+        print(self.__board__.__str__())
+        
+    def test_atack(self):
+        self.__board__ = Board()
+        self.__pawn__white__ = Pawn("WHITE")
+        self.__pawn__black__ = Pawn("BLACK")
+        self.__bishop__ = Bishop("WHITE")
+
+        self.__pawn__white__.move(6, 3, 4, 3, self.__board__)
+        self.__pawn__black__.move(1, 7, 2, 7, self.__board__)
+        
+        result = self.__bishop__.move(7,2,2,7,self.__board__)
+        self.assertEqual(result, True)
+        print(self.__board__.__str__())
+        
