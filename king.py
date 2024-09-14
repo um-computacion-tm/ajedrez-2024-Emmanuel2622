bd1228ebeb1f3792a2c3fc0cf670b866bb8add40
Carlos Emmanuel Denis <ce.denis@alumno.um.edu.ar>
2024-09-13 22:28:43 -0300

    Se creo el movimiento Vertical de la pieza King

--- king.py
+++ king.py
@@ -6,3 +6,9 @@ class King(Piece):
                 return "♔"
         else:
              return "♚"
+        
+    def type_move(self, from_row, from_col, to_row, to_col, board):
+        if abs(from_row - to_row) == 1 or (from_col - to_col) == 1 and board.get_piece(to_row, to_col).__color__ != self.__color__:
+            return True
+        
+            
