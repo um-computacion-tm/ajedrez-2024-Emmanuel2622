9b0a56e32e4133a3cba81a8682aa0354c48d4f46
Carlos Emmanuel Denis <ce.denis@alumno.um.edu.ar>
2024-09-15 01:22:16 -0300

    Se modifico un test.

--- test_rook.py
+++ test_rook.py
@@ -56,16 +56,21 @@ class Test_Rook(unittest.TestCase):
         self.assertEqual(result, 'Error en el movimiento')
         print(self.__board__.__str__())
 
+
     def test_move_horizontal_bad(self):
         self.__board__ = Board()  
         self.__rook__ = Rook("WHITE")
         self.__pawn_white__ = Pawn("WHITE")
         self.__pawn_black__ = Pawn("BLACK")
 
-        self.__pawn_white__.move(6,0,5,0,self.__board__)
+        self.__pawn_white__.move(6,0,4,0,self.__board__)
         self.__pawn_black__.move(1,0,2,0,self.__board__)
-        
-        result = self.__rook__.move(7,0,7,5, self.__board__)
+        self.__pawn_white__.move(6,1,5,1,self.__board__)
+        self.__pawn_black__.move(1,1,2,1,self.__board__)
+        self.__rook__.move(7,0,5,0, self.__board__)
+        self.__pawn_black__.move(2,1,3,1,self.__board__)
+
+        result = self.__rook__.move(5,0,5,3, self.__board__)
         self.assertEqual(result, 'Error en el movimiento')
         print(self.__board__.__str__())
 if __name__ == "__main__":
